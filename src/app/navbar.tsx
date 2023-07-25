60971c5e86c9c039442be6f5a1fe5053528ae1d4
Federico Jimenez <ricojimenez89@gmail.com>
2023-07-25 00:09:45 -0400

    added procedures link

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -2,30 +2,33 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <header>
+    <div className="navbar py-4">
       <nav className="flex justify-center space-x-4">
         <Link
           href="/"
-          className="font-bold px-3 py-2 rounded-lg hover:bg-slate-300 hover:text-slate-900"
+          className="font-bold text-white px-3 py-2 rounded-lg hover:bg-slate-300 hover:text-slate-900"
         >
           Home
         </Link>
         <Link
           href="/contact"
-          className="font-bold px-3 py-2 rounded-lg hover:bg-slate-300 hover:text-slate-900"
+          className="font-bold text-white px-3 py-2 rounded-lg hover:bg-slate-300 hover:text-slate-900"
         >
           Contact Us
         </Link>
         <Link
-          href="/login"
-          className="font-bold px-3 py-2 rounded-lg hover:bg-slate-300 hover:text-slate-900"
+          href="/procedures"
+          className="font-bold text-white px-3 py-2 rounded-lg hover:bg-slate-300 hover:text-slate-900"
         >
-          Login
+          Procedures
         </Link>
       </nav>
-      <h1 className="text-3xl font-bold flex justify-center">
+      <h2 className="text-3xl text-white font-bold flex justify-center ml-4 p-2">
         Bowen Pain Center
-      </h1>
-    </header>
+      </h2>
+      <h3 className="text-1xl text-white font-bold flex justify-center ml-4 p-2">
+        Interventional Pain Management
+      </h3>
+    </div>
   );
 }
