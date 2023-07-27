889957e97cbbce6f4b9af4c56ff22e24dfcd225e
Federico Jimenez <ricojimenez89@gmail.com>
2023-07-26 21:33:25 -0400

    cleaned up code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className}text-slate-800 mx-auto`}>
+      <body className={`${inter.className}`}>
         <Navbar />
         {children}
       </body>
