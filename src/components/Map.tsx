d17d65fadf5ba06b713571d46f8c4dd92b0d4eea
Federico Jimenez <ricojimenez89@gmail.com>
2023-07-26 21:35:49 -0400

    fixed map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,51 +1,52 @@
 "use client";
-import React from "react";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import React, { useState, useEffect, useCallback } from "react";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { GoogleMap, useJsApiLoader, MarkerF } from "@react-google-maps/api";
 
 const containerStyle = {
-  width: "400px",
+  width: "100%",
   height: "400px",
 };
 
-const center = {
-  lat: -3.745,
-  lng: -38.523,
+const clinic = {
+  lat: 40.72527,
+  lng: -73.8483,
 };
 
-function MyComponent() {
+export default function MyComponent() {
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
   });
 
-  const [map, setMap] = React.useState(null);
+  const [map, setMap] = useState(null);
 
-  const onLoad = React.useCallback(function callback(map) {
+  const onLoad = useCallback(function callback(map: any) {
     // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    const bounds = new window.google.maps.LatLngBounds(center);
+    const bounds = new window.google.maps.LatLngBounds(clinic);
     map.fitBounds(bounds);
 
     setMap(map);
   }, []);
 
-  const onUnmount = React.useCallback(function callback(map) {
+  const onUnmount = useCallback(function callback(map: any) {
     setMap(null);
   }, []);
 
   return isLoaded ? (
     <GoogleMap
       mapContainerStyle={containerStyle}
-      center={center}
-      zoom={10}
+      center={clinic}
+      zoom={18}
       onLoad={onLoad}
       onUnmount={onUnmount}
     >
       {/* Child components, such as markers, info windows, etc. */}
-      <></>
+      <MarkerF position={clinic} />
     </GoogleMap>
   ) : (
     <></>
   );
 }
 
-export default React.memo(MyComponent);
+export const Map = React.memo(MyComponent);
