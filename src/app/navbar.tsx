9e3e73e8b2bded3e675dcca67e7b90779f53f044
Federico Jimenez <ricojimenez89@gmail.com>
2023-07-26 21:34:31 -0400

    changed styling

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className="navbar py-4">
+    <div className="navbar">
       <nav className="flex justify-center space-x-4">
         <Link
           href="/"
@@ -11,22 +11,22 @@ export default function Navbar() {
           Home
         </Link>
         <Link
-          href="/contact"
-          className="font-bold text-white px-3 py-2 rounded-lg hover:bg-slate-300 hover:text-slate-900"
-        >
-          Contact Us
-        </Link>
-        <Link
           href="/procedures"
           className="font-bold text-white px-3 py-2 rounded-lg hover:bg-slate-300 hover:text-slate-900"
         >
           Procedures
         </Link>
+        <Link
+          href="/contact"
+          className="font-bold text-white px-3 py-2 rounded-lg hover:bg-slate-300 hover:text-slate-900"
+        >
+          Contact Us
+        </Link>
       </nav>
-      <h2 className="text-3xl text-white font-bold flex justify-center ml-4 p-2">
+      <h2 className="text-3xl text-white font-bold flex justify-center mx-auto p-2">
         Bowen Pain Center
       </h2>
-      <h3 className="text-1xl text-white font-bold flex justify-center ml-4 p-2">
+      <h3 className="text-1xl text-white font-bold flex justify-center mx-auto p-2">
         Interventional Pain Management
       </h3>
     </div>
