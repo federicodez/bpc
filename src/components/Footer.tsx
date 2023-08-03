8a951f6427b1386be0b69822c0150a366e608a43
Federico Jimenez <federico@Federicos-MacBook-Pro.local>
2023-08-02 20:14:25 -0400

    created footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="bg-blue-950 text-white text-center">
+      <p>
+        @2020 Bowen Pain Center
+        <br />
+        All rights reserved &copy;
+      </p>
+    </footer>
+  );
+}
