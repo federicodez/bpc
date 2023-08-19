4434ac4c16e81d5bfe4fff1d32e05ed954b39ddd
Federico Jimenez <federico@Federicos-MacBook-Pro.local>
2023-08-19 17:11:46 -0400

    rendering procedure video

--- src/app/components/Carousel.tsx
+++ src/app/components/Carousel.tsx
@@ -11,6 +11,15 @@ type Procedures = {
 export default function Carousel({ title, content }: Procedures) {
   const [active, setActive] = useState(2);
   const count = procedures.length;
+  const video = (
+    <iframe
+      className="card-content"
+      width="480"
+      height="360"
+      src="//www.youtube.com/embed/MZLocncorVg"
+      allow="accelerometer;autoplay;clipboard-write;encrypted-media;gyroscope;picture-in-picture;web-share"
+    ></iframe>
+  );
 
   return (
     <div className="carousel">
@@ -35,7 +44,9 @@ export default function Carousel({ title, content }: Procedures) {
         >
           <div className="card">
             <h2>{title}</h2>
-            <p>{content}</p>
+            <div className="flex justify-center items-center">
+              {content.length ? content : video}
+            </div>
           </div>
         </div>
       ))}
@@ -47,35 +58,3 @@ export default function Carousel({ title, content }: Procedures) {
     </div>
   );
 }
-
-// className={`card-container [var(--active):${
-//   i === active
-// }?1:0][var(--offset):(${active - i})/3] [var(--direction):Math.sign(${
-//   active - i
-// })] [--abs-offset:Math.abs(${active - i})/3] pointer-events-[${
-//   active === i
-// }?auto:none] opacity-[Math.abs(${active - i})>=3?0:1] [Math.abs(${
-//   active - i
-// })>3?hidden:block]`}
-
-// const addActive = async (e: HTMLDivElementElement): void => {
-//   const option = e.target;
-//   const options = document.body.querySelectorAll(".option");
-//   options.forEach((el: HTMLDivElement): void => {
-//     el.classList.remove("active");
-//   });
-//   option.classList.add("active");
-// };
-//         <iframe
-//           className="card-content"
-//           width="480"
-//           height="360"
-//           src="//www.youtube.com/embed/MZLocncorVg"
-//           allow="accelerometer;
-// autoplay;
-// clipboard-write;
-// encrypted-media;
-// gyroscope;
-// picture-in-picture;
-// web-share"
-//         ></iframe>
