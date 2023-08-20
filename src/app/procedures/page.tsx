6b55e51a3082feed8b2c9d0c60476e2afc63bf97
Federico Jimenez <federico@Federicos-MacBook-Pro.local>
2023-08-19 22:54:38 -0400

    grid styling

--- src/app/procedures/page.tsx
+++ src/app/procedures/page.tsx
@@ -5,26 +5,27 @@ import Carousel from "../components/Carousel";
 
 export default async function Procedures() {
   return (
-    <div className="procedures m-auto pt-20 justify-center">
-      <h1 className="linethrough m-20 text-2xl text-center font-bold text-white">
+    <div className="container py-20 mx-auto justify-center">
+      <h1 className="title m-10 text-2xl text-center font-bold text-blue-800">
         OUR PROCEDURES
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:mx-80">
+      <div className="grid justify-items-center gap-4 md:w-[800px] mx-auto my-10 grid-cols-1 md:grid-cols-2">
         <Image
-          className="procedure-image object-contain w-auto h-auto border-8 border-white drop-shadow-lg lg:w-96"
+          className="procedure-image object-contain w-96 h-full border-8 border-solid border-white 
+          shadow-[inset_0_-3em_3em_rgba(0,0,0,0.1),0_0_0_2px_rgb(255,255,255),0.3em_0.3em_1em_rgba(0,0,0,0.3)]"
           src={operation}
           priority={true}
           alt="procedure image"
         />
         <div>
-          <p className="m-4 text-white">
+          <p className="m-4">
             At Bowen Pain Center, our unwavering dedication lies in assisting
             patients through a comprehensive interventional pain management
             program. Embracing the forefront of medical progress, we offer
             advanced diagnosis and treatment techniques to ensure the best
             possible outcomes.
           </p>
-          <p className="m-4 text-white">
+          <p className="m-4">
             Our core mission at Bowen Pain Center is to relieve acute to chronic
             pain and foster improved health, comfort, and quality of life within
             a compassionate and collaborative environment. Through continuous
