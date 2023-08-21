ca9d9ded63e6b2a265b8e8f45713fef2aabc6ad7
Federico Jimenez <federico@Federicos-MacBook-Pro.local>
2023-08-20 21:44:11 -0400

    added new components

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,5 +1,7 @@
 import Maps from "./Map";
 import Navbar from "./navbar";
 import Footer from "./Footer";
+import Form from "./Form";
+import Carousel from "./Carousel";
 
-export { Maps, Navbar, Footer };
+export { Maps, Navbar, Footer, Form, Carousel };
