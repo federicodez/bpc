76a87f6da7d31154fa920d9672efaeb2a31ae8ae
Federico Jimenez <federico@Federicos-MacBook-Pro.local>
2023-08-20 22:50:12 -0400

    removed async and type from script tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,9 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${poppins.className} ${inter.className}`}>
         <Script
-          async
           defer
-          type="text/javascript"
           src={`//maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`}
         ></Script>
         <Navbar />
